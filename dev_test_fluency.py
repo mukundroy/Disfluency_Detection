373ab396b9f571bda1fe92822b487e1af11cc6c2
Mukund Roy <invincible.roy@gmail.com>
2023-12-05 16:16:33 +0530

    Initial commit

---
+++ dev_test_fluency.py
@@ -0,0 +1,130 @@
+import nltk
+import sklearn_crfsuite
+from sklearn.model_selection import train_test_split
+from sklearn_crfsuite.metrics import flat_classification_report
+from sklearn_crfsuite.metrics import flat_f1_score
+import joblib
+
+
+def read_conll_file(file_path):
+    sentences = []
+    with open(file_path, 'r', encoding='utf-8') as file:
+        sentence = []
+        for line in file:
+            line = line.strip()
+            if line:
+                word, pos_tag, defl_tag = line.split('\t')
+                sentence.append((word, pos_tag, defl_tag))
+            else:
+                if sentence:
+                    sentences.append(sentence)
+                    sentence = []
+        if sentence:
+            sentences.append(sentence)
+    return sentences
+
+
+# Define features for CRF model
+def word2features(sent, i):
+    word = sent[i][0]
+    pos = sent[i][1]
+    disfluence = sent[i][2]
+    # first word
+    if i == 0:
+        prevword = '<START>'
+        prevpos = '<START>'
+    else:
+        prevword = sent[i - 1][0]
+        prevpos = sent[i - 1][1]
+
+    # first word
+    if i == 0 or i == 1:
+        prev2word = '<START>'
+        prev2pos = '<START>'
+    else:
+        prev2word = sent[i - 2][0]
+        prev2pos = sent[i - 2][1]
+
+    # last word
+    if i == len(sent) - 1:
+        nextword = '<END>'
+        nextpos = '<END>'
+    else:
+        nextword = sent[i + 1][0]
+        nextpos = sent[i + 1][1]
+
+    # suffixes and prefixes
+    pref_1, pref_2, pref_3, pref_4 = word[:1], word[:2], word[:3], word[:4]
+    suff_1, suff_2, suff_3, suff_4 = word[-1:], word[-2:], word[-3:], word[-4:]
+
+    #    rule_state = rule_based_tagger.tag([word])[0][1]
+
+    return {'word': word,
+            # 'pref_1': pref_1,
+            # 'pref_2': pref_2,
+            # 'pref_3': pref_3,
+            # 'pref_4': pref_4,
+            # 'suff_1': suff_1,
+            # 'suff_2': suff_2,
+            # 'suff_3': suff_3,
+            # 'suff_4': suff_4,
+            'pos': pos,
+            'prevword': prevword,
+            'prevpos': prevpos,
+            'nextword': nextword,
+            'nextpos': nextpos,
+            'prev2word': prev2word,
+            'prev2pos': prev2pos
+            }
+
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+
+def sent2labels(sent):
+    return [disfluence for word, postag, disfluence in sent]
+
+
+def sent2tokens(sent):
+    return [word for word, postag in sent]
+
+
+# Path to your CoNLL format file
+file_path = '/root/home_cdac/ICON/data/disfluency/Marathi/marathi_dev_postagged.tsv'
+
+# Read tagged sentences from the CoNLL format file
+tagged_sentences = read_conll_file(file_path)
+# print(tagged_sentences[0])
+# Prepare data for training
+X = [sent2features(sent) for sent in tagged_sentences]
+y = [sent2labels(sent) for sent in tagged_sentences]
+# print(X[0])
+# print(y[0])
+# Split data into training and testing sets
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create CRF model
+model_file_path = '/root/home_cdac/ICON/data/disfluency/Marathi/crf.model'
+crf = loaded_model = joblib.load(model_file_path)
+
+y_pred = crf.predict(X)
+labels = list(crf.classes_)
+
+print(flat_f1_score(y, y_pred, average='weighted', labels=labels))
+# Print classification report
+report = flat_classification_report(y, y_pred)
+print(report)
+
+output_file_path = '/root/home_cdac/ICON/data/disfluency/Marathi/dev_test_output.txt'
+
+with open(output_file_path, 'w', encoding='utf-8') as output_file:
+    for i, sent_tags in enumerate(y_pred):
+        # print(sent_tags)
+        for j, tag in enumerate(sent_tags):
+            word = X[i][j]['word']
+            pos = X[i][j]['pos']
+            disfluence = y[i][j]
+            output_file.write(f"{word}\t{pos}\t{disfluence}\t{tag}\n")
+            # output_file.write(f"{word}\t{tag}\n")
+        output_file.write('\n')
---
+++ postag_data.py
@@ -0,0 +1,42 @@
+import nltk
+from nltk.tag import tnt
+from nltk.corpus import indian
+
+# Download the Indian language POS tagged corpus
+nltk.download('indian')
+
+# Load the Hindi POS tagged corpus
+tagged_sentences = indian.tagged_sents('bangla.pos')
+
+# Train the tagger using the Indian language corpus
+tnt_pos_tagger = tnt.TnT()
+tnt_pos_tagger.train(tagged_sentences)
+
+# Function to tag tokens in Hindi
+def hindi_pos_tag(tokens):
+    return tnt_pos_tagger.tag(tokens)
+
+# Function to read and tag tokens from a CoNLL format file and write to another file
+def tag_conll_file(file_path, output_file):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        with open(output_file, 'w', encoding='utf-8') as output:
+            for line in file:
+                line = line.strip()
+                if not line:  # Check for blank lines
+                    output.write('\n')
+                    continue
+                
+                parts = line.split('\t')
+                if len(parts) >= 2:
+                    word = parts[0]
+                    tagged_word = hindi_pos_tag([word])
+                    output.write(f"{parts[0]}\t{tagged_word[0][1]}\t{parts[1]}\n")
+            output.write('\n')  # Ensure a blank line at the end if the input file ends without one
+
+# Path to your CoNLL format file
+input_file_path = '/root/home_cdac/ICON/data/disfluency/Marathi/marathi_dev.tsv'
+# Path to the output file
+output_file_path = '/root/home_cdac/ICON/data/disfluency/Marathi/marathi_dev_postagged.tsv'
+
+# Tag tokens in the CoNLL format file and write to the output file
+tag_conll_file(input_file_path, output_file_path)
